dffbc910fb0628484c9bb678400b0efceceff0da
khaly ruot <khalyruot12345@gmail.com>
2021-06-14 04:40:48 -0400

    update

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 function myFunction1() {
     var a = document.getElementById("box").value = 1;
+    console.log(a);
 }
+myFunction1()
+
+alert(a);
+
 function myFunction2() {
     var b = document.getElementById("box").value = 2;
 }
+
 
 function myFunction3() {
     var c = document.getElementById("box").value = 3;
